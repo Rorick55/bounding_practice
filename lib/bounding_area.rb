40df102973ac0b5b290a9fac280e27e3cb5740a1
TeamRory <rorycteam@gmail.com>
2014-06-07 11:33:07 -0400

    Initialized bounding area.

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,2 +1,21 @@
 class BoundingArea
+  def initialize(boxes)
+    @boxes = boxes
+  end
+
+  def contains_point?(x,y)
+    if @boxes.empty?
+      false
+    else
+      @boxes.each do |box|
+        if box.contains_point?(x,y)
+          true
+          break
+        else
+          false
+        end
+      end
+    end
+  end
+
 end
